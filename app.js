c6f582bb382e5ff1d9e9d64278c7f8185eb7b1c4
jeevanr03 <babu.jeevan6@gmail.com>
2024-02-11 16:05:43 +0530

    Check remote repo.

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ const head = React.createElement("div", {id: "heading"},
 [React.createElement("h1", {id: "heading1"}, "Hello React1"), React.createElement("h2", {id: "heading2"}, "Hello React2")]
 )
 const root1 = ReactDOM.createRoot(document.getElementById("header"))
-root1.render(head)
+root1.render(head) 
