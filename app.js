14d395775b3e33ca993aea5baf384ffb8720468a
jeevanr03 <74764877+jeevanr03@users.noreply.github.com>
2024-02-11 16:00:36 +0530

    Initial commit for namaste-react.

---
+++ app.js
@@ -0,0 +1,11 @@
+const heading = React.createElement("div", {id: "heading"}, 
+[React.createElement("h1", {id: "heading1"}, "Hello React1"), React.createElement("h2", {id: "heading2"}, "Hello React2")]
+)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
+
+const head = React.createElement("div", {id: "heading"}, 
+[React.createElement("h1", {id: "heading1"}, "Hello React1"), React.createElement("h2", {id: "heading2"}, "Hello React2")]
+)
+const root1 = ReactDOM.createRoot(document.getElementById("header"))
+root1.render(head)
